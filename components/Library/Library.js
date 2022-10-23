0fa9822400b07c27ea9a0a476f314b5d0c6e643e
Daniel <danielukoha101@gmail.com>
2022-10-23 13:38:57 +0100

    fixing deployment issues

--- components/Library/Library.js
+++ components/Library/Library.js
@@ -1,8 +1,3 @@
-import MusicPlayer from "../Collections/MusicPlayer";
-import Header from "../Layout/Header/Header";
-import Layout from "../Layout/Layout";
-import Menu from "../Layout/Menu/Menu";
-
 const Library = () => {
   return;
 };
